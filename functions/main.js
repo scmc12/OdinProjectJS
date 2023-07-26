f2ab133e2b7c98815936a381c734d558ac0b07b4
mcgh12 <mcguire.holt@yahoo.com>
2023-07-26 11:58:12 -0500

    more problem solving

--- functions/main.js
+++ functions/main.js
@@ -29,3 +29,19 @@ lastLetter = (str) => {
 };
 
 console.log(lastLetter("holiday"));
+
+// Write a program that takes a user’s input and prints the numbers from one to the number the user entered. However, for multiples of three print Fizz instead of the number and for the multiples of five print Buzz. For numbers which are multiples of both three and five print FizzBuzz.
+
+let answer = parseInt(prompt("Please enter a number."));
+
+for (let i = 0; i <= answer; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("Fizzbuzz");
+  } else if (i % 3 === 0) {
+    console.log("fizz");
+  } else if (i % 5 === 0) {
+    console.log("buzz");
+  } else {
+    console.log(i);
+  }
+}
